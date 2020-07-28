985ecb52cd77e4a446310f5a2dd36b73f360838f
Lassi Eloranta <lassi.eloranta@gmail.com>
2020-07-29 01:11:55 +0300

    Finnish segment 19

---
+++ bin/eu/translation_fi.c
@@ -0,0 +1,15 @@
+// SM64 (EU) Segment 19 - Finnish
+
+#include "macros.h"
+
+#include "game/ingame_menu.h"
+
+#include "make_const_nonconst.h"
+
+// Include text/define_text.inc.c, preprocessed with -I text/us/ to get the
+// right translation strings, with symbols renamed as below.
+#define seg2_course_name_table course_name_table_eu_fi
+#define seg2_act_name_table act_name_table_eu_fi
+#define seg2_dialog_table dialog_table_eu_fi
+
+#include "text/fi/define_text.inc.c"
